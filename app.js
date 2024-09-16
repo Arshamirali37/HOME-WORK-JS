a7cff8359850b66a24d6be31804aa9ad336b5734
ARSHamirali <arshamirali604@gmail.com>
2024-09-16 13:07:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,160 @@
+//Chapter #9
+//Q1
+// var A =prompt("Enter your city name")
+// if(A === "karachi"){
+//     console.log("welcome user")
+// }
+
+//Q2
+// var A =prompt("Enter your gender")
+// if(A ==="male"){
+//     console.log("welcome sir")
+// }
+// if(A === "Female"){
+//     console.log(" welcome ma'am")
+// }
+
+//Q3
+// var A=prompt("Input traffic colors")
+// if(A == "Red"){
+//     console.log("Stop")
+// }
+// if(A == "Yellow"){
+//     console.log("ready")
+// }
+// if(A == "Green"){
+//     console.log("GO")
+// }
+// else{
+//     console.log("Incorrect Input")
+// }
+
+//Q4
+// var A =+prompt("Enter your remaining fuel in car","liter")
+// if(A ==0.25){
+//     console.log("Refill it")
+// }
+// else{
+//     console.log("Good to go")
+// }
+
+//Q5
+//Working
+// a. var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// Not working
+// b. var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// } False
+
+// if (c === 13){
+// alert("condition 2 is true");
+// } Ture
+
+// if (++c < 14){
+// alert("condition 3 is true");
+// } Flase
+
+// if(c === 14){
+// alert("condition 4 is true");
+// } True
+
+// d. var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }Display
+
+// Not display
+//  if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// Not display
+// f. if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// Q6
+// document.write("<h1>Marks Sheet </h1>")
+// var totalmarks=100
+// document.write("Total Marks =",totalmarks)
+// var marksobtain=+prompt("Enter student Marks")
+// document.write("<br><br> Marks Obtain =",marksobtain)
+// var papermark=100
+// var percentage=(marksobtain/papermark)*totalmarks
+// document.write(" <br><br> Percentage =",percentage)
+// if(percentage > 50){
+//     document.write("<br><br> D")
+// }
+// if(percentage > 60){
+//     document.write("<br><br> C")
+// }
+// if(percentage >70){
+//     document.write("<br><br> B")
+// }
+// if(percentage < 80){
+//     document.write("<br><br> A")
+// }
+// if(percentage >90){
+//     document.write("<br><br> A+")
+// }
+// if(percentage >90<100){
+//     document.write("<br><br> A+")
+// }
+
+//Q7
+// var A =3
+// var B=+prompt("Enter any number between 1 to 10")
+// if( B ==A){
+// console.log("Bingo!!")
+// } 
+// if(B == A+1){
+//     console.log("CLOSE")
+// }
+// else{
+//     console.log("TRY AGAIN")
+//}
+
+//Q8
+// var A=prompt("Enter any number")
+// if(A/3){
+//     console.log("number is divisible by 3.")
+// }
+// else{
+//     console.log("try again")
+// }
+
+// Q10
+// var A = +prompt("Enter any number")
+// if (A!=2) {
+//     console.log("ODD")
+// }
+// else{
+//     console.log("EVEN")
+// }
+
+// Q11
+// var A=+prompt("Enter firstnumber")
+// var B=+prompt("Enter secondnumber")
+// var C= A + B
+// document.write(C)
+// var D=A-B
+// document.write("<br>",D)
+// var E=A*B
+// document.write("<br>",E)
+// var F=A/B
+// document.write("<br>",F)
